0606beada7b87dfe349c006bae28f4fa8dd5a073
LuPhuong <hauhunghuy1998@gmail.com>
2018-04-11 19:30:31 +0700

    update

---
+++ 1/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        object locker = new object();
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            createThread(pictureBox1);
+            MoveLeft((object)pictureBox2);
+        }
+        private void createThread3(PictureBox pcb)
+        {
+            Thread thread = new Thread(MoveLeft2);
+            thread.IsBackground = true;
+            thread.Start((object)pcb);
+
+        }
+        private void createThread2(PictureBox pcb)
+        {
+            Thread thread = new Thread(MoveLeft2);
+            thread.IsBackground = true;
+            thread.Start((object)pcb);
+
+        }
+        private void createThread(PictureBox pcb)
+        {
+            Thread thread = new Thread(MoveLeft);
+            thread.IsBackground = true;
+            thread.Start((object)pcb);
+
+        }
+        private void MoveLeft2(object Picture)
+        {
+            Monitor.Enter(locker);
+            PictureBox pic = Picture as PictureBox;
+            while (true)
+            {
+                pic.Left += 10;
+                Application.DoEvents();
+                if (pic.Right >= panel1.Width - 50)
+                {
+                    break;
+                }
+                System.Threading.Thread.Sleep(50);
+            }
+            Monitor.Exit(locker);
+
+        }
+
+
+
+
+        private void MoveLeft(object Picture)
+        {
+
+            PictureBox pic = Picture as PictureBox;
+            while (true)
+            {
+                pic.Left += 10;
+                Application.DoEvents();
+                if (pic.Right >= panel1.Width - 50)
+                {
+                    break;
+                }
+                System.Threading.Thread.Sleep(50);
+            }
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Location = new Point(0, pictureBox1.Location.Y);
+            pictureBox2.Location = new Point(0, pictureBox2.Location.Y);
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            createThread2(pictureBox1);
+            MoveLeft2(pictureBox2);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            createThread(pictureBox1);
+            createThread(pictureBox2);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            createThread2(pictureBox1);
+            createThread2(pictureBox2);
+        }
+    }
+}
---
+++ DemoDaluong/DemoDaluong/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace DemoDaluong
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+           // Display();
+           // Display2();
+        }
+        object locker = new object();
+        public void Display()
+        {
+            new Thread(() =>
+            {
+                while (true)
+                {
+                    label6.Visible = true;
+                    label6.ForeColor = Color.Blue;
+
+                    Thread.Sleep(300);
+                    label6.Visible = true;
+                    label6.ForeColor = Color.Black;
+                    Thread.Sleep(500);
+
+                    
+                }
+            }).Start();
+            new Thread(() =>
+            {
+                while (true)
+                {
+                    label1.Visible = true;
+                    label1.ForeColor = Color.Green;
+
+                    Thread.Sleep(300);
+                    label1.Visible = true;
+                    label1.ForeColor = Color.Black;
+                    Thread.Sleep(500);
+                }
+            }).Start();
+        }
+       public void Display2()
+        {
+            Monitor.Enter(locker);
+            new Thread(() =>
+            {
+                while (true)
+                {
+                    label6.Visible = true;
+                    label6.ForeColor = Color.Yellow;
+                    Thread.Sleep(300);
+                    label6.Visible = true;
+                    label6.ForeColor = Color.Black;
+                    Thread.Sleep(500);
+                }
+                
+            }).Start();
+            new Thread(() =>
+            {
+                while (true)
+                {
+                    label1.Visible = true;
+                    label1.ForeColor = Color.Red;
+
+                    Thread.Sleep(300);
+                    label1.Visible = true;
+                    label1.ForeColor = Color.Black;
+                    Thread.Sleep(500);
+                }
+            }).Start();
+            Monitor.Exit(locker);
+        }
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Display();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Display2();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+    }
+}
